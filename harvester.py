b1a834bb44db3af1f48450e20c7bc23e18d7c827
Ben Stahl <48197017+benastahl@users.noreply.github.com>
2023-03-28 12:57:07 -0400

    Update harvester.py

--- harvester.py
+++ harvester.py
@@ -1,25 +1,25 @@
 import threading
-import time
-import bcolors
 import urllib3
-import re
 import colorama
 import os
-import gzip
 import uuid
-import shutil
+import zipfile
+import chromedriver_autoinstaller
 
 from datetime import datetime
 from termcolor import colored
 
-from selenium.common.exceptions import TimeoutException, InvalidArgumentException
+from selenium.common.exceptions import InvalidArgumentException, TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
-from seleniumwire import webdriver
-from chTools import chTools
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver as selenium_webdriver
+import _utils
 
 colorama.init()
+# chromedriver_autoinstaller.install(path=os.getcwd() + _utils.fd + "chromedrivers", no_ssl=True)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-chTools = chTools()
 
 token_lock, captcha_lock = threading.Lock(), threading.Lock(),
 
@@ -30,48 +30,23 @@ tokens = {}
 
 
 class Harvester:
-    def __init__(self, proxy, profile_name):
+    def __init__(self, proxy, num, profile_name):
         self.driver = None
-        self.number = 0
-        self.proxy = self.proxy_config(proxy)
+        self.num = num
+        self.proxy = proxy
         self.profile_name = profile_name
 
-    def proxy_config(self, proxy):
-        if not proxy or proxy == "Local Host":
-            return {}
-
-        parsed_proxy = None
-        try:
-            (IPv4, Port, username, password) = proxy.split(':')
-            ip = IPv4 + ':' + Port
-            parsed_proxy = {
-                "proxy": {
-                    "http": "http://" + username + ":" + password + "@" + ip,
-                    "https": "http://" + username + ":" + password + "@" + ip,
-                }
-            }
-        except ValueError:
-            self.log("Invalid proxy.", "f")
-            exit(1)
-        return parsed_proxy
-
     def log(self, text, status):
-        if status == 's':
+        statuses = {
+            "s": 'green',
+            "f": "red",
+            "p": "cyan",
+            "d": "yellow",
+        }
+        if status in statuses:
             print(colored(
-                f"[{datetime.now().strftime('%m-%d-%Y %H:%M:%S')}] - [{self.number + 1}] - [Captcha Harvester] - {text}",
-                'green'))
-        if status == 'f':
-            print(colored(
-                f"[{datetime.now().strftime('%m-%d-%Y %H:%M:%S')}] - [{self.number + 1}] - [Captcha Harvester] - {text}",
-                'red'))
-        if status == 'p':
-            print(colored(
-                f"[{datetime.now().strftime('%m-%d-%Y %H:%M:%S')}] - [{self.number + 1}] - [Captcha Harvester] - {text}",
-                'cyan'))
-        if status == 'd':
-            print(colored(
-                f"[{datetime.now().strftime('%m-%d-%Y %H:%M:%S')}] - [{self.number + 1}] - [Captcha Harvester] - {text}",
-                'yellow'))
+                f"[{datetime.now().strftime('%m-%d-%Y %H:%M:%S')}] - [{self.num}] - [Captcha Harvester] - {text}",
+                statuses[status]))
 
     @staticmethod
     def token_needed():
@@ -114,14 +89,69 @@ class Harvester:
         </html>
 
 
-                """ % (self.number, self.profile_name)
+                """ % (self.num, self.profile_name)
         self.driver.get("data:text/html;charset=utf-8," + waiting_html)
 
     def open(self):
-        executable_path = chTools.get_chromedriver_path()
-        chrome_options = chTools.profile_arguments(Options(), profile_name=self.profile_name)
-        self.driver = webdriver.Chrome(executable_path=executable_path, options=chrome_options,
-                                       seleniumwire_options=self.proxy)
+        chrome_options = _utils.profile_arguments(Options(), profile_name=self.profile_name)
+
+        # Proxy Setting (extension)
+        if self.proxy:
+            manifest_json = """
+            {
+                "version": "1.0.0",
+                "manifest_version": 2,
+                "name": "Chrome Proxy",
+                "permissions": [
+                    "proxy",
+                    "tabs",
+                    "unlimitedStorage",
+                    "storage",
+                    "<all_urls>",
+                    "webRequest",
+                    "webRequestBlocking"
+                ],
+                "background": {
+                    "scripts": ["background.js"]
+                },
+                "minimum_chrome_version":"22.0.0"
+            }
+            """
+            background_js = """
+            var config = {
+                    mode: "fixed_servers",
+                    rules: {
+                    singleProxy: {
+                        scheme: "http",
+                        host: "%s",
+                        port: parseInt(%s)
+                    },
+                    bypassList: ["localhost"]
+                    }
+                };
+            chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+            function callbackFn(details) {
+                return {
+                    authCredentials: {
+                        username: "%s",
+                        password: "%s"
+                    }
+                };
+            }
+            chrome.webRequest.onAuthRequired.addListener(
+                        callbackFn,
+                        {urls: ["<all_urls>"]},
+                        ['blocking']
+            );
+            """ % (_utils.proxy_config(self.proxy))
+            pluginfile = 'proxy_auth_plugin.zip'
+
+            with zipfile.ZipFile(pluginfile, 'w') as zp:
+                zp.writestr("manifest.json", manifest_json)
+                zp.writestr("background.js", background_js)
+            chrome_options.add_extension(pluginfile)
+
+        self.driver = selenium_webdriver.Chrome(chrome_options=chrome_options)
         self.waiting()
 
     def wait_for_captcha(self):
@@ -144,163 +174,82 @@ class Harvester:
                     continue
                 token_lock.release()
         except InvalidArgumentException:
-            menu(
-                message=bcolors.FAIL + 'Make sure you do not have any browsers open with the same browser profile.' + bcolors.END)
             self.driver.quit()
+            raise InvalidArgumentException('Make sure you do not have any browsers open with the same browser profile.')
 
     def get_valid_token(self, captcha_type):
-        while True:
-            self.log("Waiting for Captcha...", "p")
+        self.log("Waiting for Captcha...", "p")
 
-            try:
-                version_paths = {
-                    "v2": "/recaptcha/api2/userverify",
-                    "v3": "/recaptcha/enterprise/reload"
-                }
-                request = self.driver.wait_for_request(pat=version_paths[captcha_type], timeout=10000)
-
-                response_body = str(gzip.decompress(request.response.body)).replace('"', '')
-                response_list = response_body.split(',')
-                recaptcha_token = response_list[1]
-                invalid_v2_token = re.findall("bgdata", response_body) and captcha_type == "v2"
-                if invalid_v2_token:
-                    self.log("Invalid recaptcha token.", "F")
-                    del self.driver.requests
-                    continue
-
-                self.log('Valid token found', "s")
-                del self.driver.requests
-                return recaptcha_token
-
-            except TimeoutError or TimeoutException:
-                menu(message=bcolors.FAIL + 'Chrome Harvester timed out. Restart and try again.' + bcolors.END)
-            except Exception as exc:
-                chTools.clear()
-                self.log("Error occurred: " + str(exc), "F")
-                time.sleep(2)
-
-
-def chrome_login():
-    driver = None
-    try:
-        while True:
-            profile_name = chTools.question(name="Profile Name", message="Profile name:")
-            try:
-                if profile_name in os.listdir(chTools.get_profiles_path("")):
-                    print(
-                        bcolors.FAIL + "Failed to create new browser profile. Try a different name." + bcolors.END)
-                    continue
-
-                print(bcolors.BLUE + "Opening with new user data..." + bcolors.ENDC)
-                login_args = chTools.profile_arguments(Options(), profile_name=profile_name)
-
-                driver = webdriver.Chrome(options=login_args, executable_path=chTools.get_chromedriver_path())
-                break
-
-            except FileExistsError:
-                print(bcolors.FAIL + 'That profile name already exists. Choose another.' + bcolors.END)
-            # except WebDriverException:
-            #     print(bcolors.FAIL + "Chromedriver version is OUT OF DATE. Replace with latest stable version." + bcolors.END)
-
-        print(bcolors.BLUE + "Enter your login information." + bcolors.ENDC)
-        driver.get('https://gmail.com')
+        frame_titles = {
+            "v2": "recaptcha challenge expires in two minutes",
+            "v3": "reCAPTCHA"
+        }
         try:
-            del driver.requests
-            input("Please press enter once you have logged in: ")
+            frame = WebDriverWait(self.driver, 20).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, f"iframe[title='{frame_titles[captcha_type]}']")))
+            self.driver.switch_to.frame(frame)
         except TimeoutError or TimeoutException:
-            menu(message=bcolors.FAIL + "Recaptcha Harvester Login timed out. Try again." + bcolors.ENDC)
+            self.driver.refresh()
+            return self.get_valid_token(captcha_type)
 
-        chTools.save_browser_profile(profile_name)
-        driver.quit()
-        menu(
-            message=bcolors.OK + 'Profile Save "' + bcolors.BOLD + bcolors.UNDERLINE + profile_name + bcolors.ENDC + bcolors.OK + '" completed!' + bcolors.ENDC)
-    except InvalidArgumentException:
-        menu(
-            message=bcolors.FAIL + "Make sure you do not have any browsers open with the same browser profile." + bcolors.ENDC)
+        recaptcha_token = None
+        while not recaptcha_token:
+            recaptcha_token = self.driver.find_element(By.ID, "recaptcha-token").get_attribute('value')
+
+        self.log('Valid token found', "s")
+        return recaptcha_token
 
 
-def browser_profiles():
+def chrome_login(profile_name: str):
     try:
-        # Choose browser profile to edit list
-        profiles = os.listdir(chTools.get_profiles_path(""))
-        profile_name = chTools.question(name="Browser Profile List", message="Select a browser profile:",
-                                        choices=profiles)
+        try:
+            if profile_name in os.listdir(_utils.get_profiles_path("")):
+                raise AssertionError(
+                    "Failed to create new browser profile. Try a different name.")
 
-        # Edit options
-        edit_answer = chTools.question(name="EditOptions", message="Select an edit option:",
-                                       choices=['Change name', 'Delete', 'Go back'])
+            print("Opening with new user data...")
+            login_args = _utils.profile_arguments(selenium_webdriver.ChromeOptions(), profile_name=profile_name)
 
-        if edit_answer == "Go back":
-            chTools.clear()
-            browser_profiles()
+            driver = selenium_webdriver.Chrome(options=login_args)
 
-        if edit_answer == 'Change name':
-            while True:
-                profile_rename = chTools.question(name="Rename",
-                                                  message='Enter a new name for the browser profile:')
+        except FileExistsError:
+            raise FileExistsError('That profile name already exists. Choose another.')
+        # except WebDriverException:
+        #     print("Chromedriver version is OUT OF DATE. Replace with latest stable version.")
 
-                if profile_rename in os.listdir(chTools.get_profiles_path("")):
-                    print(bcolors.FAIL + 'That profile name already exists. Choose another.' + bcolors.END)
-                    continue
+        print("Enter your login information.")
+        driver.get('https://gmail.com')
+        input("Press enter once you have logged in: ")
 
-                og_path = chTools.get_profiles_path(profile_name)
-                rename_path = chTools.get_profiles_path(profile_rename)
-
-                # Rename
-                os.rename(og_path, rename_path)
-
-                menu(
-                    message=bcolors.OK + 'Browser Profile: "' + profile_name + '" has been renamed to: "' + profile_rename + '"' + bcolors.ENDC)
-                break
-        if edit_answer == 'Delete':
-            shutil.rmtree(chTools.get_profiles_path(profile_name))
-            menu(
-                message=bcolors.OK + 'Browser profile: "' + profile_name + '" has been deleted.' + bcolors.ENDC)
+        driver.quit()
+        print(f'Profile Save "{profile_name}" completed.')
     except InvalidArgumentException:
-        menu(
-            message=bcolors.FAIL + 'Make sure you do not have any browsers open with the same browser profile.' + bcolors.ENDC)
-    except TimeoutError or TimeoutException:
-        menu(message=bcolors.FAIL + 'Chrome Harvester timed out. Restart and try again.' + bcolors.END)
+        raise InvalidArgumentException(
+            "Make sure you do not have any browsers open with the same browser profile.")
 
 
-def open_harvester():
-    browser_profiles = os.listdir(chTools.get_profiles_path(""))
-    browser_profiles.remove("bp-proxies.json")
-    profile_name = chTools.question(name="Profile Choice", message="Choose a browser profile:",
-                                    choices=browser_profiles)
-
-    proxy_choices = ["Local Host"]
-    proxy_choices.extend(chTools.get_formatted_proxies())
-    saved_proxy = chTools.get_bp_proxy(profile_name)
-    if saved_proxy:
-        proxy_choices.insert(0, saved_proxy + " (SAVED)")
-    proxy = chTools.question(name="Proxy Choice", message="Proxy options:", choices=proxy_choices).replace(
-        " (IN USE BY ANOTHER PROFILE)", "").replace(" (SAVED)", "")
-    if saved_proxy != proxy:
-        save_proxy = chTools.question(name="Save Proxy?", message="Save Proxy/LH option?",
-                                      choices=["Yes", "No"])
-        if save_proxy == "Yes":
-            chTools.save_bp_proxy(profile_name, proxy)
+def open_harvester(profile_name: str, proxy=None):
+    browser_profiles = os.listdir(_utils.get_profiles_path(""))
+    assert profile_name in browser_profiles, "Profile not found."
 
     harvester = Harvester(
+        num=len(harvesters) + 1,
         proxy=proxy,
         profile_name=profile_name
     )
     harvesters.append(harvester)
-    harvester.number = harvesters.index(harvester)
     harvester.open()
     threading.Thread(target=harvester.wait_for_captcha, args=()).start()
-    menu()
 
 
-def harvest_token(captchaType, captchaURL):
+def harvest_token(captcha_type, url):
     print("In queue for captcha token...")
     global tokens
     with token_lock:
         task_id = uuid.uuid4()
         token_inquirers[task_id] = {
-            "type": captchaType,
-            "url": captchaURL,
+            "type": captcha_type,
+            "url": url,
         }
 
     # Enter queue to get token
@@ -313,47 +262,3 @@ def harvest_token(captchaType, captchaURL):
             tokens.pop(task_id)
 
     return g_recaptcha_token
-
-
-def menu(message=None):
-    chTools.clear()
-    print(message)
-    menu_answer = chTools.question(name="Selection", message="Main Menu",
-                                   choices=["Start Tasks", 'Chrome Login', "Open Harvester",
-                                            'Your Browser Profiles', 'Test the Captcha Harvester', 'Exit'])
-
-    if menu_answer == "Start Tasks":
-        print("Nothing here lol. Call your own function. -line329")
-    elif menu_answer == "Test the Captcha Harvester":
-        if not harvesters:
-            menu(message=bcolors.FAIL + "No open harvesters." + bcolors.ENDC)
-
-        ReCaptchaType = chTools.question(name="ReCaptcha Type", message="ReCaptcha Type:",
-                                         choices=["v2"])
-
-        tokenAmount = int(chTools.question(name="Token Amount", message="Enter token amount:"))
-
-        for harvester in harvesters:
-            threading.Thread(target=harvester.wait_for_captcha, args=()).start()
-
-        threads = []
-        for n in range(tokenAmount):
-            thread = threading.Thread(target=harvest_token, args=(ReCaptchaType, "https://www.google.com/recaptcha/api2/demo"))
-            thread.start()
-            threads.append(thread)
-
-        for thread in threads:
-            thread.join()
-
-        menu(message=bcolors.OKMSG + "Finished captcha token collection." + bcolors.ENDC)
-    elif menu_answer == "Exit":
-        for harvester in harvesters:
-            harvester.driver.quit()
-        exit(1)
-    else:
-        menu_calls = {"Chrome Login": chrome_login, "Open Harvester": open_harvester, "Your Browser Profiles": browser_profiles}
-        menu_calls[menu_answer]()  # Call menu answer func
-
-
-if __name__ == "__main__":
-    menu()
